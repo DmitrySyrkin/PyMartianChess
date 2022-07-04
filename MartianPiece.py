9af6f1c659a23cfeade21231a376372eca45540d
Dmitry Syrkin <dmitry@syrkin.com>
2022-07-05 00:52:14 +0300

    Pawn movement added

--- MartianPiece.py
+++ MartianPiece.py
@@ -4,17 +4,17 @@ PURPLE = (128,0,128)
 BANANA = (227,207,87)
 
 class MartianPiece:
-    def __init__(self, screen, center, radius, isSelected = False, isCapturedByPlayer = None):
+    def __init__(self, screen, position, radius, isSelected = False, isCapturedByPlayer = None):
         self._bounds = None        
         self._screen = screen
-        self._center = (center[0] * 60 + 30, center[1] * 60 + 30)        
         self._radius = radius
         self._isSelected = isSelected              
         self._isCapturedByPlayer = isCapturedByPlayer
+        self._position = position
 
     def draw(self):
-        if self._isCapturedByPlayer == None:
-          self._bounds = pygame.draw.circle(self._screen, self.color, self._center, self._radius)
+        if self._isCapturedByPlayer == None:          
+          self._bounds = pygame.draw.circle(self._screen, self.color, self.center, self._radius)
 
     def collidepoint(self, point):
         if self._isCapturedByPlayer != None \
@@ -24,21 +24,20 @@ class MartianPiece:
         if self._bounds.collidepoint(point):
             return True
         
-        rect = pygame.rect.Rect(self._center[0] - 30, self._center[1] - 30, 60, 60)
+        rect = pygame.rect.Rect(self.center[0] - 30, self.center[1] - 30, 60, 60)
         return rect.collidepoint(point)
 
     def select(self, isSelected):
         self._isSelected = isSelected
  
     def move(self, column, row):
-      self._center = (column * 60 + 30, row * 60 + 30) 
+      self._position = (column, row) 
 
     def capture(self, currentPlayer):
         self._isCapturedByPlayer = currentPlayer       
 
     def belongsToPlayer(self, currentPlayer: int):
-        row = (self._center[1] - 30) / 60
-        return row > 3 if currentPlayer == 0 else row < 4
+        return self.row > 3 if currentPlayer == 0 else self.row < 4
 
     @property
     def isSelected(self):
@@ -47,3 +46,19 @@ class MartianPiece:
     @property
     def color(self):
         return BANANA if self.isSelected else PURPLE    
+
+    @property
+    def pieceType(self):
+        return 'Pawn' if self._radius == 10 else ('Rook' if self._radius == 15 else 'Queen')
+
+    @property
+    def row(self):
+        return self._position[1]
+
+    @property
+    def column(self):
+        return self._position[0]
+
+    @property
+    def center(self):
+        return self.column * 60 + 30, self.row * 60 + 30
--- MoveValidator.py
+++ MoveValidator.py
@@ -1,7 +1,13 @@
 import pygame
+from MartianField import MartianField
 from MartianPiece import MartianPiece 
 
 class MoveValidator:
 
-    def IsValidMove(self, selectedPiece: MartianPiece, field: pygame.Rect, pieces: list[MartianPiece]):
+    def IsValidMove(self, selectedPiece: MartianPiece, field: MartianField, pieces: list[MartianPiece]):
+      
+      if selectedPiece.pieceType == 'Pawn':
+        return max(selectedPiece.row, field.row) - min(selectedPiece.row, field.row) == 1 \
+              and max(selectedPiece.column, field.column) - min(selectedPiece.column, field.column) == 1
+
       return True
